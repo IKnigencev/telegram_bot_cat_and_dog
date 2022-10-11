a27ef06085299ea66af5280f84a7b388823a60dd
Ivan Knigencev <74624665+IKnigencev@users.noreply.github.com>
2022-10-11 14:49:46 +0500

    Add translate compliment

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import requests
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import CommandHandler, Updater
+from googletrans import Translator
 
 
 load_dotenv()
@@ -14,9 +15,13 @@ logging.basicConfig(
     level=logging.INFO)
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+NASA_TOKEN = os.getenv('NASA_TOKEN')
 URL_CATS = 'https://api.thecatapi.com/v1/images/search'
 URL_DOGS = 'https://api.thedogapi.com/v1/images/search'
 URL_COMPLIMENT = 'https://complimentr.com/api'
+URL_NASA = 'https://api.nasa.gov/planetary/apod?{NASA_TOKEN}'
+
+translator = Translator()
 
 
 def get_cats_image():
@@ -49,12 +54,28 @@ def get_compliment():
     try:
         response = requests.get(URL_COMPLIMENT).json()
         rendom_compliment = response.get('compliment')
-        return rendom_compliment
+        result = translator.translate(rendom_compliment, src='en', dest='ru')
+        return result
     except Exception as error:
         logging.error(f'Ошибка при запросе к API комплиментов: {error}')
         response = requests.get(URL_CATS).json()
         random_compliment = response[0].get('url')
         return random_compliment
+
+
+def get_nasa_api():
+    '''Обращение к API для получения фото и описания космоса.'''
+    try:
+        response = requests.get(URL_NASA, headers=NASA_TOKEN).json()
+        title = response.get('title')
+        image = response.get('url')
+        return title, image
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к API NASA: {error}')
+        response = requests.get(URL_CATS).json()
+        title = 'Не нашли картинку космоса, но есть котики!'
+        random_cats = response[0].get('url')
+        return title, random_cats
 
 
 def new_dog(update, context):
@@ -75,12 +96,20 @@ def new_compliment(update, context):
     context.bot.send_message(chat.id, get_compliment())
 
 
+def new_space(update, context):
+    '''Функция получения данных о космосе.'''
+    chat = update.effective_chat
+    title, image = get_nasa_api()
+    context.bot.send_message(chat.id, title)
+    context.bot.send_photo(chat.id, image)
+
+
 def wake_up(update, context):
     '''Функция при активации бота.'''
     chat = update.effective_chat
     name = update.message.chat.first_name
     button = ReplyKeyboardMarkup(
-        [['/newcat', '/newdog'], ['/newcompliment']],
+        [['/newcat', '/newdog'], ['/newcompliment'], ['/newnasaimage']],
         resize_keyboard=True
     )
 
@@ -102,6 +131,7 @@ def main():
     updater.dispatcher.add_handler(
         CommandHandler('newcompliment', new_compliment)
     )
+    updater.dispatcher.add_handler(CommandHandler('newnasaimage', new_space))
 
     updater.start_polling()
     updater.idle()
