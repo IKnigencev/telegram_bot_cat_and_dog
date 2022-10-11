5b0f1ef9868eed283d9b896fc219423000300707
Ivan Knigencev <74624665+IKnigencev@users.noreply.github.com>
2022-10-11 14:07:55 +0500

    Back

--- main.py
+++ main.py
@@ -14,11 +14,9 @@ logging.basicConfig(
     level=logging.INFO)
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-NASA_TOKEN = os.getenv('NASA_TOKEN')
 URL_CATS = 'https://api.thecatapi.com/v1/images/search'
 URL_DOGS = 'https://api.thedogapi.com/v1/images/search'
 URL_COMPLIMENT = 'https://complimentr.com/api'
-URL_NASA = 'https://api.nasa.gov/planetary/apod?'
 
 
 def get_cats_image():
@@ -59,21 +57,6 @@ def get_compliment():
         return random_compliment
 
 
-def get_nasa_api():
-    '''Обращение к API для получения фото и описания космоса.'''
-    try:
-        response = requests.get(URL_NASA, headers=NASA_TOKEN).json()
-        title = response.get('title')
-        image = response.get('url')
-        return title, image
-    except Exception as error:
-        logging.error(f'Ошибка при запросе к API NASA: {error}')
-        response = requests.get(URL_CATS).json()
-        title = 'Не нашли картинку космоса, но есть котики!'
-        random_cats = response[0].get('url')
-        return title, random_cats
-
-
 def new_dog(update, context):
     '''Функция получения новой фотографии собаки.'''
     chat = update.effective_chat
@@ -92,20 +75,12 @@ def new_compliment(update, context):
     context.bot.send_message(chat.id, get_compliment())
 
 
-def new_space(update, context):
-    '''Функция получения данных о космосе.'''
-    chat = update.effective_chat
-    title, image = get_nasa_api()
-    context.bot.send_message(chat.id, title)
-    context.bot.send_photo(chat.id, image)
-
-
 def wake_up(update, context):
     '''Функция при активации бота.'''
     chat = update.effective_chat
     name = update.message.chat.first_name
     button = ReplyKeyboardMarkup(
-        [['/newcat', '/newdog'], ['/newcompliment'], ['/newnasaimage']],
+        [['/newcat', '/newdog'], ['/newcompliment']],
         resize_keyboard=True
     )
 
@@ -127,7 +102,6 @@ def main():
     updater.dispatcher.add_handler(
         CommandHandler('newcompliment', new_compliment)
     )
-    updater.dispatcher.add_handler(CommandHandler('newnasaimage', new_space))
 
     updater.start_polling()
     updater.idle()
